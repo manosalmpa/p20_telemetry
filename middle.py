a27cdb5e8963c4644709e26425c5b4ec73a0de5b
mky13 <techno_manosal@windowslive.com>
2019-11-25 00:35:43 +0200

    basic tabs

--- middle.py
+++ middle.py
@@ -8,17 +8,23 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 
+from tabs import Tabs
+
+
 Builder.load_string('''
 <Middle>:
     canvas.before:
         Color:
             rgba: 1,0,0,1
         Line:
-            rectangle: self.x, self.y, self.height/2, self.width
+            rectangle: self.x, self.y, self.width, self.height
     Label:
         pos_hint: {"top":1, "x":0.3}
-        size_hint: 0.2, 0.2
+        size_hint: 0.2, 0.1
         text: "middle"
+    Tabs:
+        pos_hint: {"top": .9, "x": 0}
+        size_hint: 1, 0.7
 ''')
 
 
